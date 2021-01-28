6e3467d8095489466d845ce5505cb0e36013ed6d
Falconboy129 <78124627+Falconboy129@users.noreply.github.com>
2021-01-27 17:54:52 -0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+function wonder1(){
+    window.alert("All three of Giza's famed pyramids and their elaborate burial complexes were built during a frenetic period of construction, from roughly 2550 to 2490 B.C. The pyramids were built by Pharaohs Khufu (tallest), Khafre (background), and Menkaure (front).
+");
+}
